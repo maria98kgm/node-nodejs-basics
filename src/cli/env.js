6f4693b834a097fc2dbd3b5976580d36a1b0e3f3
Marie Kagermanova <maria.kgm04@gmail.com>
2023-06-16 01:45:57 +0300

    feat: complete Command Line tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const logStr = [];
+
+    Object.keys(process.env).forEach((envVar) => {
+        if (/^RSS_/.test(envVar)) logStr.push(`${envVar}=${process.env[envVar]}`);
+    });
+
+    console.log(logStr.join("; "));
 };
 
 parseEnv();
