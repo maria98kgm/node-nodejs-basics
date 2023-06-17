d1a5abc36f763a6f749f0d42d965eca6c6788dd5
Marie Kagermanova <maria.kgm04@gmail.com>
2023-06-17 17:41:12 +0300

    feat: complete Hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = `${__dirname}\\files\\fileToCalculateHashFor.txt`;
+
+    fs.readFile(filePath, "utf-8", (err, data) => {
+        console.log(createHash("sha256").update(data).digest("hex"));
+    });
 };
 
 await calculateHash();
