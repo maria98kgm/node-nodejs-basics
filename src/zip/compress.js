472da16e78c5ab2c893b555b3858a4ace17577e0
Marie Kagermanova <maria.kgm04@gmail.com>
2023-06-19 00:49:25 +0300

    feat: complete Zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import zlib from "zlib";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const compressFile = `${__dirname}\\files\\fileToCompress.txt`;
+    const compressDestination = `${__dirname}\\files\\archive.gz`;
+
+    const gz = zlib.createGzip();
+    const readable = fs.createReadStream(compressFile);
+    const writable = fs.createWriteStream(compressDestination);
+
+    readable.pipe(gz).pipe(writable);
 };
 
 await compress();
